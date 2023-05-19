f62735660cc2d010c1b3ed08d08e6053345fd5fc
3012056 <leno.moraes@ifmt.edu.br>
2023-05-19 19:58:50 -0400

    ajustes 5

---
+++ teste-app.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+RESULT="'wget -qO- http://localhost:8090'"
+wget -q localhost:8090
+if [$? -eq 0]
+then
+    echo 'ok - servico no ar!'
+elif [[ $RESULT == *"Number"* ]]
+then
+    echo 'ok - numero de visitantes'
+    echo $RESULT
+else
+    echo 'tudo errado no numero de visitantes'
+    exit 1
+fi
